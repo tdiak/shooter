45be534665796f7fc8b09e50ffd23569bb415b32
Tomek Diaków <tomekdiakow@MacBook-Pro-2.local>
2019-04-19 11:23:11 +0200

    Update zombie moves

--- main.lua
+++ main.lua
@@ -10,7 +10,11 @@ function love.load()
     player.y = love.graphics.getHeight() / 2
     player.speed = 3
 
+    maxTime = 2
+    timer = 2
+
     zombies = {}
+    bullets = {}
 end
 
 
@@ -27,6 +31,13 @@ function love.update(dt)
 
     for i, zombie in ipairs(zombies) do
         zombie.x = zombie.x + math.cos(zombiePlayerAngle(zombie)) * zombie.speed
+        zombie.y = zombie.y + math.sin(zombiePlayerAngle(zombie)) * zombie.speed
+    end
+
+    timer = timer - dt
+    if timer <= 0 then
+        spawnZombie()
+        timer = maxTime
     end
 end
 
@@ -35,8 +46,12 @@ function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
     love.graphics.draw(sprites.player, player.x, player.y, mouseAngle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
 
-    for i, z in ipairs(zombies) do
-        love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(zombie), nil, nil, sprites.zombie:getWidth() / 2, sprites.zombie:getHeight() / 2)
+    for i, zombie in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, zombie.x, zombie.y, zombiePlayerAngle(zombie), nil, nil, sprites.zombie:getWidth() / 2, sprites.zombie:getHeight() / 2)
+    end
+
+    for i, b in ipairs(bullets) do
+
     end
 end
 
@@ -49,13 +64,38 @@ function mouseAngle()
 end
 
 function zombiePlayerAngle(zombie)
-    return math.atan2(zombie.y - player.y, zombie.x - player.x)
+    return math.atan2(player.y - zombie.y, player.x - zombie.x)
 end
 
 function spawnZombie()
     zombie = {}
-    zombie.x = 0
-    zombie.y = 0 
+    zombie.x = 100
+    zombie.y = 100 
     zombie.speed = 1
+    zombie.dead = false
+
+    local side = math.random(1, 4)
+
+    if side == 1 then
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 2 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = -30
+    elseif side == 3 then
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 4 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = love.graphics.getHeight() + 30
+    end   
     table.insert(zombies, zombie)
+end
+
+function spawnBullet()
+    bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = 8
+    table.insert( bullets, bullet)
 end
